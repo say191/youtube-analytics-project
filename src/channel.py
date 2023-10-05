97f828319cb05b733ba8adaacc00d119b853d57b
Igor Baydikov <igor_baidikov@mial.ru>
2023-10-05 12:53:56 +0300

    fix class

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
+import os
+from googleapiclient.discovery import build
+import json
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    apikey = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=apikey)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.info = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.info, indent=2, ensure_ascii=False))

f420fe11a39df7add1be760f7ce8f58152410b1e
Igor Baydikov <igor_baidikov@mial.ru>
2023-10-11 12:19:43 +0300

    -add __add__, __sub__ etc.

--- src/channel.py
+++ src/channel.py
@@ -13,9 +13,33 @@ class Channel:
         self.title = self.info['items'][0]['snippet']['title']
         self.description = self.info['items'][0]['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{self.info['items'][0]['id']}"
-        self.subs_count = self.info['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.info['items'][0]['statistics']['videoCount']
-        self.view_count = self.info['items'][0]['statistics']['viewCount']
+        self.subs_count = int(self.info['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.info['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.info['items'][0]['statistics']['viewCount'])
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return self.subs_count+other.subs_count
+
+    def __sub__(self, other):
+        return self.subs_count-other.subs_count
+
+    def __gt__(self, other):
+        return self.subs_count>other.subs_count
+
+    def __ge__(self, other):
+        return self.subs_count>=other.subs_count
+
+    def __lt__(self, other):
+        return self.subs_count<other.subs_count
+
+    def __le__(self, other):
+        return self.subs_count<=other.subs_count
+
+    def __eq__(self, other):
+        return self.subs_count==other.subs_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
